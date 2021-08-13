0fe65afc56f7f5feff3ec403c76be7d0b39655a1
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2021-08-13 23:00:59 +0700

    ..

--- frontend/pages/api/get-paintings-filters.js
+++ frontend/pages/api/get-paintings-filters.js
@@ -3,16 +3,40 @@ import { STRAPI_ENDPOINT } from '../../lib/constants'
 
 export default function handler(req, res)
 {
-	const { artist_id } = req.query
+	const { artist_id,painting_type,price_min,price_max } = req.query
+
+	const all = [
+		artist_id && {artist_id} || null,
+		painting_type && {painting_type} || null,
+		price_min && {price_min} || null,
+		price_max && {price_max} || null
+	]
+	const filters = []
+
+	all.filter(v=>v).map((v,k)=>{
+		if (v.artist_id)
+			filters.push(`artist: { id: "${v.artist_id}" }`)
+
+		if (v.painting_type)
+			filters.push(`painting_type : { en_name: "${v.painting_type}" }`)
+	
+		if (v.price_min)
+			filters.push(`price_gte : ${v.price_min}`)
+
+		if (v.price_max>0)
+			filters.push(`price_lte : ${v.price_max}`)
+	})
 
 	const q = gql`
 		{ paintings (
-			where: {
-				artist: { id: "${artist_id}" }
-			}
+			where: {${filters.join(',')}}
 		) {
 			id
 			vn_title
+			artist {
+				id
+				full_name
+			}
 			painting_type {
 				en_name
 			}
--- frontend/pages/paintings/filters/index.js
+++ frontend/pages/paintings/filters/index.js
@@ -1,20 +1,43 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { Breadcrumb,Container,Row,Col } from 'react-bootstrap'
 
 import { HOST_URL,STRAPI_ENDPOINT } from '../../../lib/constants'
-import { Tag } from '../../../components'
+import { Tag,PaintingListInfinite } from '../../../components'
 import styles from '../../../styles/PaintingsFilters.module.scss'
+
+const filtersQuery = (filters) =>
+{
+	const parseFilters = [
+		filters.painting_type && { painting_type:filters.painting_type } || null,
+		filters.price && {price_min:filters.price[0],price_max:filters.price[1]} || null
+	]
+	const mutateFilters = []
+
+	parseFilters.filter(v=>v).map((v,k)=>{
+		if (v.painting_type)
+			mutateFilters.push(`painting_type=${v.painting_type}`)
+
+		if(v.price_min>=0)
+			mutateFilters.push(`price_min=${v.price_min}&price_max=${v.price_max}`)
+	})
+
+	return fetch(`/api/get-paintings-filters?${mutateFilters.join('&')}`)
+		.then(res=>res.json())
+}
 
 const PaintingsFilters = ({typesData}) =>
 {
-	// console.log(typesData)
-	const [filters,setFilters] = useState({ type:null,price:null })
-	const [result,setResult] = useState([])
+	const [filters,setFilters] = useState({ painting_type:null,price:[0,0] })
+	const [results,setResults] = useState([])
+	const router = useRouter()
 
 	const pricerangeList = [
-		[1000,5000],
-		[5000,100000]
+		[0,5000000],
+		[5000000,25000000],
+		[25000000,100000000],
+		[100000000,-1]
 	]
 
 	return (
@@ -35,9 +58,13 @@ const PaintingsFilters = ({typesData}) =>
 							return <Tag
 								key={k}
 								text={v.en_name}
-								itemOnClick={
-									()=>setFilters({ ...filters,type:v.en_name })
-								}
+								itemOnClick={()=>{
+									const _ = { ...filters,painting_type:v.en_name }
+									filtersQuery(_).then(data=>{
+										setFilters(_)
+										setResults(data.paintings)
+									})
+								}}
 							/>
 						})
 					}
@@ -49,18 +76,27 @@ const PaintingsFilters = ({typesData}) =>
 						pricerangeList.map((v,k)=>{
 							return <Tag
 								key={k}
-								text={`${v[0]} - ${v[1]}`}
-								itemOnClick={
-									()=>setFilters({ ...filters,price:v[0] })
-								}
+								text={`${v[0]} - ${ v[1]>0 ? v[1] : '+' }`}
+								itemOnClick={()=>{
+									const _ = { ...filters,price:v }
+									filtersQuery(_).then(data=>{
+										setFilters(_)
+										setResults(data.paintings)
+									})
+									
+								}}
 							/>
 						})
 					}
 				</div>
 
 				<h6>Filter Result</h6>
-				<div>{filters.type||'-'} | {filters.price||'-'}</div>
-				<div>---</div>
+				{
+					results.length
+					? <PaintingListInfinite items={results} itemOnClick={ e=>router.push(`/paintings/d/${e.id}`) } />
+					: 'nothing show'
+				}
+				
 			</div>
 
 		</Container>
