c32ba75538baf81269fb04b65400a92ccaaae1da
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2021-09-09 00:18:18 +0700

    ....

--- frontend/pages/cart/index.js
+++ frontend/pages/cart/index.js
@@ -1,6 +1,17 @@
+import { useSelector } from 'react-redux'
+import { Breadcrumb,Container,Row,Col } from 'react-bootstrap'
+
 const Cart = () =>
 {
-	return 'Cart'
+	const cart = useSelector((state) => state.cart)
+	console.log(cart)
+	return (
+		<Container className="p-3 pt-5">
+			<Breadcrumb>
+				<Breadcrumb.Item active>Your Cart</Breadcrumb.Item>
+			</Breadcrumb>
+		</Container>
+	)
 }
 
 export default Cart
